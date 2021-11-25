4319e2890f04b95382e0969e30033fb9e4ef06f7
Jayden Sim <39719057+immutableshrub@users.noreply.github.com>
2021-11-25 14:39:56 +1100

    First Commit

---
+++ index.js
@@ -0,0 +1,104 @@
+import { readFileSync } from "fs";
+import { createServer } from 'https';
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from 'uuid';
+
+const ShitDataStorageSolution = {
+    ioClients: {},
+    ioDocuments: {},
+    ioOTKeys: {},
+    ForwardedEvents: ["SharedStateRelay-DSMG-ioComm"],
+}
+
+const httpServer = createServer(
+    {
+        key: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert-key.pem"),
+        cert: readFileSync("C:/Certs/ssc/LocalServer_Cert1/create-cert.pem")
+    },
+    (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        switch (req.url) {
+            case '/collab/server/info':
+                res.writeHead(200);
+                res.end(JSON.stringify({
+                    servertime: new Date().getTime(),
+                    status: 200,
+                    version: "1.0.0",
+                    socketAddr: '/socket',
+                    security: 'none'
+                }));
+                break;
+            default:
+                res.writeHead(404);
+                res.end();
+                break;
+        }
+    });
+
+const io = new Server(httpServer, {
+    path: "/socket",
+    serveClient: false,
+    cors: {
+        origin: ["https://stride-frontend.vercel.app", "https://localhost:1673", "https://192.168.86.50:1673"],
+    }
+});
+
+io.engine.generateId = (req) => {
+    return uuidv4();
+}
+
+io.on("connection", (socket) => {
+    console.log(socket.id + ' has joined.');
+    socket.on('SharedStateRelay-DSMG-ioComm', (docID, name, args) => {
+        console.log('Recieved Event from ' + ShitDataStorageSolution.ioClients[socket.id].rProfileID + ': ' + docID + ' ' + name + ' ' + args);
+        io.to(ShitDataStorageSolution.ioClients[socket.id]).emit('SharedStateRelay-DSMG-ioComm', name, args);
+    });
+});
+
+io.of(/(.*?)/).on("connection", (socket) => {
+    console.log(socket.id + ' has joined.');
+    socket.data.userProfile = socket.handshake.query.userprofile;
+
+    socket.nsp.emit('SharedStateRelay-DSMG-ioConnectivity-NewUser', JSON.parse(socket.handshake.query.userprofile));
+
+    socket.on("SharedStateRelay-DSMG-ioConnectivityCheck", (callback) => {
+        socket.nsp.emit('SharedStateRelay-DSMG-ioDocumentStateUpdateGet')
+        socket.nsp.fetchSockets().then((sockets) => {
+            const intl = {
+                currentUsers: []
+            }
+            sockets.forEach(socket => {
+                intl.currentUsers.push(JSON.parse(socket.data.userProfile));
+            })
+            callback(intl);
+        });
+    });
+
+
+    socket.on('SharedStateRelay-DSMG-ioDocumentStateUpdate', (documentState) => {
+        console.log('got documentState')
+        socket.nsp.emit('SharedStateRelay-DSMG-ioDocumentStateUpdate', documentState)
+    });
+
+    socket.on('SharedStateRelay-DSMG-ioComm', (name, args) => {
+        socket.nsp.emit('SharedStateRelay-DSMG-ioComm', name, args);
+    });
+
+    socket.on('SharedStateRelay-DSMG-ioComm', (name, args) => {
+        socket.nsp.emit('SharedStateRelay-DSMG-ioComm', name, args);
+    });
+
+    socket.on("disconnecting", (reason) => {
+        for (const room of socket.rooms) {
+            if (room !== socket.id) {
+                socket.to(room).emit("user has left", socket.id);
+            }
+        }
+    });
+});
+
+httpServer.listen(1674);
+console.log("Server started");
+
+
